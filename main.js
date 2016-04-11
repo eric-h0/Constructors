4aa21541341950091ec2fd859f8da658414032b4
E-Hizzle <eho.rutgers@gmail.com>
2016-04-11 17:51:53 -0400

    Almost finished, have to complete bus function.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var colors = require('colors');
 var Student = require('./student.js');
 
 prompt.start();
-prompt.get(['Name', 'Gender', 'Grade', 'Detentions', 'sleepingInClass', 'catchPhrase'],function(err,result){
+prompt.get(['Name', 'Gender', 'Grade', 'GPA', 'Detentions', 'sleepingInClass', 'catchPhrase'],function(err,result){
+  var student = new Student(result.Name, result.Gender, result.Grade, result.GPA, result.Detentions, result.sleepingInClass, result.catchPhrase);
+  student.canStudentHaveFun();
+});
 
-})
+module.exports = Student;
--- student.js
+++ student.js
@@ -1,13 +1,23 @@
+var main = require('./main.js');
+
+
 var Student = function(name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase){
-  this.name = name;
-  this.gender = gender;
-  this.grade = grade; 
-  this.GPA = GPA;
-  this.detentions = detentions;
+  this.name            = name;
+  this.gender          = gender;
+  this.grade           = grade; 
+  this.GPA             = GPA;
+  this.detentions      = detentions;
   this.sleepingInClass = sleepingInClass;
-  this.catchPhrase = catchPhrase;
+  this.catchPhrase     = catchPhrase;
 
   this.canStudentHaveFun = function(){
-    //Figure this out later.
-  }
-}
+    if ((this.detentions < 10) && (this.GPA > 2)){
+      console.log("Student can have fun.")
+    }
+    else {
+      console.log("Student cannot have fun. Do better.")
+    }
+  };
+};
+
+module.exports = Student;
