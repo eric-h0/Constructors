b132409c96714074ae9f335d6760dcf05f1f55ab
E-Hizzle <eho.rutgers@gmail.com>
2016-04-14 14:03:56 -0400

    Finished bus.js.

--- bus.js
+++ bus.js
@@ -1,14 +1,18 @@
-var Bus = function(studentsOnTheBus, driverName, color, gas, studentEntersBus, busChatter){
-  this.studentsOnTheBus = [];
-  this.driverName = "";
-  this.color = "";
-  this.gas = 0;
-  this.studentsOnTheBus = function(){
-    //Think about this.
-  }
-  this.busChatter = function(){
-    console.log(Student.catchPhrase);
-  }
+var Bus = function(studentsOnTheBus, driverName, color, gas, studentEntersBus, busChatter) {
+    this.studentsOnTheBus = [];
+    this.driverName = "";
+    this.color = "";
+    this.gas = 0;
+    this.studentsOnTheBus = function() {
+        this.studentsOnTheBus.push(new Student(name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase));
+        fs.appendFile("schoolbus.txt", JSON.stringify(this.studentsOnTheBus), function(err) {
+            if (err)
+                throw err;
+        })
+    }
+    this.busChatter = function() {
+        console.log(Student.catchPhrase);
+    }
 }
 
 module.exports = Bus;
