76015c7558b1b1c628b0e4136545be630ce926b6
E-Hizzle <eho.rutgers@gmail.com>
2016-04-14 14:00:37 -0400

    Working on bus portion.

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ var Bus = require('./bus.js');
 prompt.start();
 prompt.get(['Name', 'Gender', 'Grade', 'GPA', 'Detentions', 'sleepingInClass', 'catchPhrase'],function(err,result){
   var student = new Student(result.Name, result.Gender, result.Grade, result.GPA, result.Detentions, result.sleepingInClass, result.catchPhrase);
-  student.canStudentHaveFun();
+    student.canStudentHaveFun();
 });
